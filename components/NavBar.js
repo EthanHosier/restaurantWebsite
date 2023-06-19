b0b83bf8620dbc290c3daa2d70fb6fddac9698f3
Ethan Hosier <ethanjhosier@gmail.com>
2023-06-19 20:56:12 +0100

    added scroll to top button

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,7 +8,7 @@ import NavbarItems from "./NavbarItems";
 import Logo from "/public/logo.png"
 import DATA from "/public/CONSTANTS.json"
 import Hamburger from 'hamburger-react'
-
+import { BiChevronUp } from "react-icons/bi"
 //how many pixels need to scroll down before navbar dissapears
 const SCROLL_TOLERANCE = 100;
 
@@ -17,18 +17,25 @@ const Navbar = () => {
     const [isVisible, setIsVisible] = useState(true);
     const [isFullScreenNavVisible, setisFullScreenNavVisible] = useState(false);
     const [startScrollPosition, setStartScrollPosition] = useState(0)
+    const [srollToTopVisible, setScrollToTopVisible] = useState(false);
 
     const handleScroll = () => {
         const mainElement = document.getElementById('parallaxContainer');
         const currentScrollPos = mainElement.pageYOffset || mainElement.scrollTop;
 
-        
+        if (currentScrollPos > 500) {
+            setScrollToTopVisible(true);
+        } else {
+            setScrollToTopVisible(false);
+        }
+
+        //nav logic
         const isVisible = currentScrollPos <= 200 || prevScrollPos > currentScrollPos || currentScrollPos < startScrollPosition + SCROLL_TOLERANCE;
-        if(prevScrollPos > currentScrollPos) setStartScrollPosition(currentScrollPos);
-        
+        if (prevScrollPos > currentScrollPos) setStartScrollPosition(currentScrollPos);
+
         setPrevScrollPos(currentScrollPos);
         setIsVisible(isVisible);
-        
+
     };
 
     useEffect(() => {
@@ -59,69 +66,89 @@ const Navbar = () => {
         },
     };
 
+    const handleScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth', // Use smooth scrolling behavior
+            duration: 10, // Adjust the duration as per your preference (in milliseconds), not working for some reason
+        });
+    };
+
+    const iconSize = Math.min(56, window.innerWidth / 12);
+
     return (
-        <nav className={`fixed top-0 left-0 w-screen bg-primary transition-transform duration-300 ease-in-out z-10 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
-            <div className='flex justify-center shadow'>
-                <div className='flex justify-between items-center p-4 md:px-8 lg:px-12 max-w-7xl flex-1'>
-                    <Link href={"/"}>
-                        <Image src={Logo} width={0} className="w-20 md:w-28" alt='Logo' />
-                    </Link>
-                    <div className='hidden md:flex flex-1 justify-around max-w-2xl'>
-                        {DATA.navOptions.map((p, i) => {
-                            return (
-                                <div className='dropdown' key={i}>
-                                    {
-                                        p.dropdownOptions?.length > 0 ?
-                                            <>
-                                                <p className='text-tprimary uppercase'>{p.name}</p>
-                                                <div className='dropdown-container '>
-                                                    <div className='dropdown-contents bg-primary shadow-xl whitespace-nowrap'>
-                                                        {p.dropdownOptions.map((d, i) => (
-                                                            <Link href={p.url + d.url} key={i}>
-                                                                <p className=''>{d.name}</p>
-                                                            </Link>
-                                                        ))}
+        <>
+            <nav className={`fixed top-0 left-0 w-screen bg-primary transition-transform duration-300 ease-in-out z-10 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
+                <div className='flex justify-center shadow'>
+                    <div className='flex justify-between items-center p-4 md:px-8 lg:px-12 max-w-7xl flex-1'>
+                        <Link href={"/"}>
+                            <Image src={Logo} width={0} className="w-20 md:w-28" alt='Logo' />
+                        </Link>
+                        <div className='hidden md:flex flex-1 justify-around max-w-2xl'>
+                            {DATA.navOptions.map((p, i) => {
+                                return (
+                                    <div className='dropdown' key={i}>
+                                        {
+                                            p.dropdownOptions?.length > 0 ?
+                                                <>
+                                                    <p className='text-tprimary uppercase'>{p.name}</p>
+                                                    <div className='dropdown-container '>
+                                                        <div className='dropdown-contents bg-primary shadow-xl whitespace-nowrap'>
+                                                            {p.dropdownOptions.map((d, i) => (
+                                                                <Link href={p.url + d.url} key={i}>
+                                                                    <p className=''>{d.name}</p>
+                                                                </Link>
+                                                            ))}
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </>
+                                                </>
 
-                                            :
+                                                :
 
-                                            <Link href={p.url}>
-                                                <p className='text-tprimary uppercase'>{p.name}</p>
-                                            </Link>
-                                    }
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <Link href={DATA.bookUrl}>
-                        <p className='bg-accent rounded text-tsecondary p-3 px-4 text-sm hidden md:block font-semibold'>
-                            BOOK A TABLE
-                        </p>
-                    </Link>
-                    <div className={`md:hidden z-10 right-4 ${isFullScreenNavVisible ? "fixed" : "absolute"}`}>
-                        <Hamburger toggled={isFullScreenNavVisible} toggle={setisFullScreenNavVisible} />
+                                                <Link href={p.url}>
+                                                    <p className='text-tprimary uppercase'>{p.name}</p>
+                                                </Link>
+                                        }
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <Link href={DATA.bookUrl}>
+                            <p className='bg-accent rounded text-tsecondary p-3 px-4 text-sm hidden md:block font-semibold'>
+                                BOOK A TABLE
+                            </p>
+                        </Link>
+                        <div className={`md:hidden z-10 right-4 ${isFullScreenNavVisible ? "fixed" : "absolute"}`}>
+                            <Hamburger toggled={isFullScreenNavVisible} toggle={setisFullScreenNavVisible} />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* Overlay */}
-            <AnimatePresence>
-                {isFullScreenNavVisible && (
-                    <motion.div
-                        className="navbar bg-primary"
-                        initial="hidden"
-                        animate={isFullScreenNavVisible ? "visible" : "hidden"}
-                        exit="hidden"
-                        variants={navContainer}
-                    >
-                        <NavbarItems isToggled={isFullScreenNavVisible} close={() => setisFullScreenNavVisible(false)} />
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                {/* Overlay */}
+                <AnimatePresence>
+                    {isFullScreenNavVisible && (
+                        <motion.div
+                            className="navbar bg-primary"
+                            initial="hidden"
+                            animate={isFullScreenNavVisible ? "visible" : "hidden"}
+                            exit="hidden"
+                            variants={navContainer}
+                        >
+                            <NavbarItems isToggled={isFullScreenNavVisible} close={() => setisFullScreenNavVisible(false)} />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
 
-        </nav>
+            </nav>
+            <button
+                className={`fixed z-10 bottom-0 right-4 md:right-16 md:bottom-8 rounded-full p-2 bg-accent drop-shadow-lg flex items-center justify-center transition-transform duration-300 ease-in-out ${srollToTopVisible ? '-translate-y-8' : 'translate-y-20 md:translate-y-32'}`}
+                onClick={handleScrollToTop}
+            >
+                <Link href={"/"}>
+                    <BiChevronUp color='white' size={iconSize} />
+                </Link>
+            </button>
+        </>
     );
 };
 
