f373cb23c59dac2cfe5ecad182feb17c8711d9a3
Ethan Hosier <ethanjhosier@gmail.com>
2023-06-24 15:59:54 +0100

    added min heights for parallax image (for when phone rotated)

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
       </section>
 
       {/* (Pickup & delivery section) */}
-      <section className='normal h-2/3 flex-col' id='pickup-delivery'>
+      <section className='normal flex-col' id='pickup-delivery'>
         <img src={DATA.backgrounds.pickupDeliverySection} className="background" alt='pick up and delivery background / extra image' />
         <div className='bg-black opacity-20 background' />
         <div className=' h-full flex flex-col md:justify-around'>
@@ -71,7 +71,7 @@ export default function Home() {
       </section>
 
       {/* Extra parallax image section */}
-      <section className='parallax flex smaller-parallax-section' id='extra-parallax-image'>
+      <section className='parallax flex smaller-parallax-section'>
         <img src={DATA.backgrounds.extraParaPic} className="background" alt='extra booking section background' />
         <div className='bg-black background opacity-20' />
         <h1 className='text-tsecondary text-center -mt-20'>"{DATA.slogan}"</h1>
