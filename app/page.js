7eb18afa0b1c3504ac03d631b87bdc8eaa99f302
EthanHosier <ethanjhosier@gmail.com>
2023-07-05 00:27:21 +0100

    added revalidate .env in pagejs app

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ import Link from 'next/link'
 import PhotoGallery from '@/components/PhotoGallery';
 import { getWebsiteData } from '@/util/util';
 
-export const revalidate = process.env.REVALIDATE_SECS // revalidate every 3 hours
+export const revalidate = process.env.REVALIDATE_SECS 
 
 const LOGO_MAP = { tripadvisor: Tripadvisor, google: GoogleLogo, yelp: YelpLogo }
 
