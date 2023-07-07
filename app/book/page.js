9aefb37cca619e0b992c7ebf12a06fe1a4363643
EthanHosier <ethanjhosier@gmail.com>
2023-07-07 14:31:19 +0100

    made iframe taller = fixed overdlow????

--- app/book/page.js
+++ app/book/page.js
@@ -16,7 +16,7 @@ const page = async() => {
       <section className='normal bg-primary min-h-screen w-screen' id="bookingPage">
         <iframe
           src={`https://d303vz01x9nm1g.cloudfront.net?restaurantId=${process.env.WEBSITE_UUID}`}
-          className="shadow-xl h-[750px] mt-12 sm:mt-16 w-full max-w-xl border rounded-lg overflow-hidden"
+          className="shadow-xl h-[800px] mt-12 sm:mt-16 w-full max-w-xl border rounded-lg overflow-hidden"
           id="booking-iframe"
         />
       </section>
